88df239e5cbca334cf73caa818006bace9465043
Jesse De Loore <jesse.deloore@external.engie.com>
2023-09-07 16:34:10 +0200

    fix version to use in other CI's

--- flask_restless_datamodel/__init__.py
+++ flask_restless_datamodel/__init__.py
@@ -6,4 +6,4 @@ from . import patches  # noqa
 from .datamodel import DataModel  # noqa
 
 # Check the PBR version module docs for other options than release_string()
-__version__ = "0.2.12"
+__version__ = VersionInfo('flask-restless-datamodel').release_string()
